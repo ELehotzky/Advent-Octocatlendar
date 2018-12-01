9d7ed09b3e07d3b9612e8ec73006b37083fe44ee
no-relation <eddiec76@gmail.com>
2018-12-01 14:02:19 -0600

    rendered cat cards

--- js/app.js
+++ js/app.js
@@ -149,8 +149,13 @@ function newDeck(catCards) {
 
 const catDeck = newDeck(catCards)
 
+// original version
+// function makeCard(card) {
+// 	return `<li class="card" data-card="${card}"><i class="fa ${card}"></i></li>`;
+// }
+
 function makeCard(card) {
-	return `<li class="card" data-card="${card}"><i class="fa ${card}"></i></li>`;
+	return `<li class="card" data-card="${card.day}"><img src="${card.relLink}" height="125" width="125" alt="${card.title()}" title="by ${card.creator}"></li>`;
 }
 
 /*
@@ -168,7 +173,7 @@ function makeCard(card) {
 function startGame() {
 	removeBanner();
 	const deck = document.querySelector(".deck");
-	let cardHTML = cards.map(function(card) {
+	let cardHTML = catDeck.map(function(card) {
 		return makeCard(card);
 	});
 	deck.innerHTML = cardHTML.join("");
